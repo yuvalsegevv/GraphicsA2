e1aed5eea77dd248194cb5cd00656203aac76838
nave avraham <103640905+Naveavra@users.noreply.github.com>
2024-02-15 10:04:40 +0200

    Update game.cpp added parser function for input files

--- Game/game.cpp
+++ Game/game.cpp
@@ -1,6 +1,17 @@
 #include "game.h"
 #include <iostream>
+#include <fstream>
 #include <glm/gtc/matrix_transform.hpp>
+
+
+static std::vector<std::tuple<std::string, glm::vec4>> hitObjects;
+static std::vector<glm::vec4> eyeCoords;
+static std::vector<glm::vec4> spotLights;
+static std::vector<glm::vec4> directLights;
+static std::vector<glm::vec4> lightIntens;
+static std::vector<glm::vec4> ambientColor;
+static std::vector<glm::vec4> objColors;
+
 
 static void printMat(const glm::mat4 mat)
 {
@@ -20,6 +31,99 @@ Game::Game() : Scene()
 Game::Game(float angle ,float relationWH, float near1, float far1) : Scene(angle,relationWH,near1,far1)
 { 	
 }
+
+std::vector<std::string> split(const std::string& s, const std::string& delimiter) {
+	size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+	std::string token;
+	std::vector<std::string> res;
+
+	while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+		token = s.substr(pos_start, pos_end - pos_start);
+		pos_start = pos_end + delim_len;
+		res.push_back(token);
+	}
+
+	res.push_back(s.substr(pos_start));
+	return res;
+}
+
+void initVec(glm::vec4& vec, std::vector<std::string> coords)
+{
+	vec.x = std::stof(coords[0]);
+	vec.y = std::stof(coords[1]);
+	vec.z = std::stof(coords[2]);
+	vec.w = std::stof(coords[3]);
+}
+
+void processLine(const std::string& line) {
+	if (line.empty()) return; // Skip empty lines
+
+	char type = line[0]; // First character indicates the type
+	std::string data = line.substr(2); // The rest of the line is data
+	std::vector<std::string> coords;
+	std::string typeVal;
+	glm::vec4 vec;
+	switch (type) {
+	case 'e': // EYE VEC
+		coords = split(data, " ");
+		initVec(vec, coords);
+		eyeCoords.push_back(vec);
+		break;
+	case 'a': // AMBIENT VEC
+		coords = split(data, " ");
+		initVec(vec, coords);
+		ambientColor.push_back(vec);
+		break;
+	case 'o': // OBJECT VEC
+	case 'r': // REFLECTIVE OBJECT VEC
+	case 't': // TRANSPARENT OBJECT VEC
+		type == 'o' ? typeVal = "object" : (type == 'r' ? typeVal = "reflective" : typeVal = "transparent");
+		coords = split(data, " ");
+		initVec(vec, coords);
+		hitObjects.push_back(std::make_tuple(typeVal, vec));
+		break;
+	case 'c': // COLOR OF OBJECT VEC
+		coords = split(data, " ");
+		initVec(vec, coords);
+		objColors.push_back(vec);
+		break;
+	case 'd': // DIRECT LIGHT VEC
+		coords = split(data, " ");
+		initVec(vec, coords);
+		directLights.push_back(vec);
+		break;
+	case 'p': // SPOTLIGHT VEC
+		coords = split(data, " ");
+		initVec(vec, coords);
+		spotLights.push_back(vec);
+		break;
+	case 'i': // LIGHT INTENSITY VEC
+		coords = split(data, " ");
+		initVec(vec, coords);
+		lightIntens.push_back(vec);
+		break;
+	default:
+		std::cerr << "Unknown line type: " << type << std::endl;
+		break;
+	}
+}
+
+void parseFile()
+{
+	std::string currLine;
+	std::ifstream inputFile("../res/scene1.txt", std::ios::in);
+	if (!inputFile)
+	{
+		std::cerr << "could not open the specify file" << std::endl;
+		exit(1);
+	}
+	while (std::getline(inputFile, currLine))
+	{
+		processLine(currLine);
+	}
+
+}
+
 
 void Game::Init()
 {		
